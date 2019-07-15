c63854a41975d77fcc5808ce6c9f00277367c25d
vladislav <ya.vladicl@ya.ru>
2019-07-15 22:34:30 +0300

    Hello world

---
+++ IT_School.CSharp.HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace IT_School.CSharp.HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Read();
+        }
+    }
+}
