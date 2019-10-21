c5eb4ec81d078b68b6244ac696b1728b488b7d38
vladislav <ya.vladicl@ya.ru>
2019-10-21 19:26:14 +0300

    21.10

---
+++ IT_School.CSharp.DI/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IT_School.CSharp.DI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+         
+            serviceCollection.AddSingleton<Single>();
+
+            var provider = serviceCollection.BuildServiceProvider();
+
+            var single = provider.GetService<Single>();
+            Console.WriteLine(single.S);
+
+            Console.ReadKey();
+        }
+    }
+
+    public class Single
+    {
+        public string S => "I Single";
+    }
+}
