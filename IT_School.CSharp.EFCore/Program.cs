ddcfe64db96caf7040531610e60eaa786a63c765
vladislav <ya.vladicl@ya.ru>
2019-10-28 19:17:40 +0300

    28.10

---
+++ IT_School.CSharp.EFCore/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace IT_School.CSharp.EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddDbContext<DatabaseContext>(options =>
+            {
+                options.UseNpgsql("User ID=postgres;Password=123456;host=localhost;Database=it_school;");
+            });
+            
+            var provider = serviceCollection.BuildServiceProvider();
+
+            using (var scope = provider.CreateScope())
+            {
+                var dbContext = scope.ServiceProvider.GetService<DatabaseContext>();
+                
+                var person = dbContext.Persons.First();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
